270e845923567499c4d97d904fbb72fce2facd3c
Hyeonjoon Kim <hyeon00n@naver.com>
2024-10-23 15:54:10 +0900

    Add NumberBaseBall class for game logic

---
+++ src/NumberBaseBall.java
@@ -0,0 +1,14 @@
+public class NumberBaseBall {
+
+    public void controller() {
+    }
+
+    public void numberGenerator() {
+    }
+
+    public void getUserInput() {
+    }
+
+    public void answerHandler() {
+    }
+}
