d945ba0e9646028b66178da19364e244e8d352cc
Hyeonjoon Kim <hyeon00n@naver.com>
2024-10-24 14:15:11 +0900

    feat : Add getUserInput method from NumberBaseBall class

--- src/NumberBaseBall.java
+++ src/NumberBaseBall.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class NumberBaseBall {
 
@@ -7,6 +11,10 @@ public class NumberBaseBall {
      * 플레이어가 맞춰야 하는 숫자
      */
     private final LinkedHashSet<Integer> numberOfAnswer = new LinkedHashSet<>();
+    /**
+     * 플레이어가 제출한 숫자
+     */
+    private final List<Integer> numberOfUser = new ArrayList<>();
 
     public void controller() {
     }
@@ -22,8 +30,18 @@ public class NumberBaseBall {
         }
     }
 
+    /**
+     * 사용자로부터 숫자를 입력을 받고 분석 가능한 형태로 가공합니다.
+     */
     private void getUserInput() {
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
 
+        while (userInput > 0) {
+            numberOfUser.add( userInput % 10);
+            userInput /= 10;
+        }
+        Collections.reverse(numberOfUser);
     }
 
     private void answerHandler() {
