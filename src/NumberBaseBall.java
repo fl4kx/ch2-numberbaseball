88748efadd522ec4d0139f8254f35bbd0928e8a4
Hyeonjoon Kim <hyeon00n@naver.com>
2024-10-24 13:40:58 +0900

    feat : Add numberGenerator from NumberBaseBall class

--- src/NumberBaseBall.java
+++ src/NumberBaseBall.java
@@ -1,14 +1,31 @@
+import java.util.LinkedHashSet;
+import java.util.Random;
+
 public class NumberBaseBall {
+
+    /**
+     * 플레이어가 맞춰야 하는 숫자
+     */
+    private final LinkedHashSet<Integer> numberOfAnswer = new LinkedHashSet<>();
 
     public void controller() {
     }
 
-    public void numberGenerator() {
+    /**
+     * 정답으로 사용될 임의의 세 자리 수를 생성합니다.
+     */
+    private void numberGenerator() {
+        Random random = new Random();
+
+        while (numberOfAnswer.size() < 3) {
+            numberOfAnswer.add(random.nextInt(9) + 1);
+        }
     }
 
-    public void getUserInput() {
+    private void getUserInput() {
+
     }
 
-    public void answerHandler() {
+    private void answerHandler() {
     }
 }
